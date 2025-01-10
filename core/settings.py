e7cc1b17237c1a93580e18af2cbb8b7ffd799ec6
Lu7474 <maxac10112004@mail.com>
2025-01-10 16:01:21 +0300

    add models and admin

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "dashboard",
     "users",
-    "products",
+    "orders",
 ]
 
 MIDDLEWARE = [
--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Products
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ["name"]
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ["name", "quantity", "price"]
+    search_fields = ["name", "description"]
+    list_filter = ["quantity", "category"]
+    fields = [
+        "name",
+        "category",
+        "slug",
+        "description",
+        "image",
+        "price",
+        "quantity",
+    ]
